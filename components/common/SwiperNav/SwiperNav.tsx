baab8fb562a9fb8cb5a526ee7d1df1fb093e9001
smerch88 <maxarmyk@gmail.com>
2023-05-01 13:57:48 +0300

    fix: slider navigation

--- components/common/SwiperNav/SwiperNav.tsx
+++ components/common/SwiperNav/SwiperNav.tsx
@@ -1,18 +1,19 @@
 import { FC } from 'react';
-import { useSwiper } from 'swiper/react';
 import cn from 'classnames';
 
 import { SwiperNavProps } from './SwiperNav.props';
 import Arrow from '@/public/images/feedback/arrow.svg';
 
-export const SwiperNav: FC<SwiperNavProps> = ({ className = '' }) => {
-  const swiper = useSwiper();
-
+export const SwiperNav: FC<SwiperNavProps> = ({
+  className = '',
+  prevRef,
+  nextRef,
+}) => {
   return (
     <div className={cn('flex items-center justify-center gap-4', className)}>
       <button
         aria-label="Previous Slide"
-        onClick={() => swiper.slidePrev()}
+        ref={prevRef}
         className={`rounded-[32px] bg-border px-4 py-3 text-dark  duration-300 hover:bg-accent hover:text-white`}
       >
         <Arrow className="h-7 w-7 rotate-180 fill-current" />
@@ -20,9 +21,7 @@ export const SwiperNav: FC<SwiperNavProps> = ({ className = '' }) => {
 
       <button
         aria-label="Next Slide"
-        onClick={() => {
-          swiper.slideNext();
-        }}
+        ref={nextRef}
         className={`rounded-[32px] bg-border px-4 py-3 text-dark  duration-300 hover:bg-accent hover:text-white`}
       >
         <Arrow className="h-7 w-7 fill-current" />
